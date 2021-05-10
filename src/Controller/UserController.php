eb1eab4993d48eb7f29a99f22cc0e7a633acce0e
Ildar Mosnabeev <web.udm@gmail.com>
2021-05-10 19:02:34 +0400

    Тестовый контроллер

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
+#[Route(path: "api", name: "user_api")]
 class UserController extends AbstractController
 {
-    #[Route(path: "/test", name: 'test')]
+    #[Route(path: "/post_user", name: 'post_user')]
     public function postUser(Request $request)
     {
-        return new JsonResponse(['ok' => 'ok']);
+        $ser = $this->get('serializer_mine');
+
+        return new JsonResponse(['t' => 't']);
+
+       /* $user = $ser->deserialize($request->getContent(), User::class, 'json');
+
+        return new JsonResponse($ser->serialize($user, 'json'));*/
     }
 }
